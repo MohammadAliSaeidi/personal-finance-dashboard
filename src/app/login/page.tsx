adf795ccad77eaea150cf938668c6d7a2b29f771
MohammadAli Saeidi <1saeidima@gmail.com>
2024-06-27 23:23:35 +0330

    chore (package.json): add react-hook-form feat (login/page.tsx): use react-hook-form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,26 @@
 "use client";
 import { FormEvent } from "react";
+import { useForm } from "react-hook-form";
+
+type Inputs = {
+	username: string;
+	password: string;
+};
 
 export default function Login() {
-	const handleOnLoginSubmit = (event?: FormEvent<HTMLFormElement>) => {
-		event?.preventDefault();
-		console.log(event);
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+	} = useForm<Inputs>();
+	const handleOnLoginSubmit = (formData: Inputs) => {
+		console.log(formData);
 	};
 
 	return (
 		<form
 			className="flex flex-col gap-6 mx-auto max-w-xs min-h-screen justify-center items-stretch"
-			onSubmit={handleOnLoginSubmit}
+			onSubmit={handleSubmit(handleOnLoginSubmit)}
 		>
 			<div className="flex flex-col gap-2">
 				<label htmlFor="username">Username</label>
@@ -18,7 +28,15 @@ export default function Login() {
 					id="username"
 					type="text"
 					className="border border-gray-400 rounded-md"
+					{...register("username", {
+						required: "Please enter your username",
+					})}
 				/>
+				{errors?.username && (
+					<span className="text-red-600">
+						{errors.username.message}
+					</span>
+				)}
 			</div>
 			<div className="flex flex-col gap-2">
 				<label htmlFor="password">Password</label>
@@ -26,7 +44,15 @@ export default function Login() {
 					id="passsord"
 					type="password"
 					className="border border-gray-400 rounded-md"
+					{...register("password", {
+						required: "Please enter your password",
+					})}
 				/>
+				{errors?.password && (
+					<span className="text-red-600">
+						{errors.password.message}
+					</span>
+				)}
 			</div>
 
 			<button
